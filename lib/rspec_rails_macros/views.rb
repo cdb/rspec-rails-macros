fd15250ae52432cb452b422d4e0ee55a298c29e9
Cameron Booth <cameron@cdbdesign.net>
2009-12-31 12:55:50 -0800

    Supporting a tag_content option at the end of the it_should_have_tag_for macro

--- lib/rspec_rails_macros/views.rb
+++ lib/rspec_rails_macros/views.rb
@@ -1,12 +1,12 @@
 module Rspec::Rails::Macros::Views
   module ExampleGroupMethods
     
-    def it_should_have_tag_for(name, tag)
+    def it_should_have_tag_for(name, tag, tag_content = nil)
       klass = self.described_type
       it "should have a #{name}" do
         do_act!
         evalled_tag = eval("\"#{tag}\"")
-        response.should have_tag(evalled_tag)
+        response.should have_tag(evalled_tag, eval("\"#{tag_content}\""))
       end
     end  
   end
